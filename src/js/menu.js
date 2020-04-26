d41f8770b39ae79b82223d09e530c41d5fd03f09
youngMaster46 <kaplin95@inbox.ru>
2020-04-26 08:09:56 +0300

    add actions for resize screen

--- src/js/menu.js
+++ src/js/menu.js
@@ -7,7 +7,7 @@ const mymenu = getFirstClassName('header-navigation')
 const createBack = () => {
     let back = document.createElement('div')
     back.style.position = 'fixed'
-    back.style.zIndex = 1
+    back.style.zIndex = 2
     back.style.top = 0
     back.style.right = 0
     back.style.bottom = 0
@@ -34,5 +34,11 @@ menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('menu-btn_active')
     mymenu.classList.toggle('header-navigation_active')
     toggleBack()
+})
+window.addEventListener('resize', () => {
+    // on resize screen return default behavior of menu and gamburger button
+    deleteBack()
+    mymenu.classList.remove('header-navigation_active')
+    menuBtn.classList.remove('menu-btn_active')
 })
 export default menuBtn
